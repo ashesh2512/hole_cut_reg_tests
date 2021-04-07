2ddea8531d579f2e2affb1a2e61a1a510c6f7410
asharma <ashesh.sharma@nrel.gov>
2021-04-07 00:02:15 -0600

    initial commit

---
+++ cpu_gpu_pathway/run_tests_cpu_gpu_pathway.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+echo ""
+echo "RUNNING CTV test with 2 CELL VAR AND 1 NODE VAR ON 1 CORE."
+echo ""
+cd /scratch/asharma/amr_reg_test/cpu_gpu_pathway/ctv
+rm -rf core* plt*
+srun -n 1 -c 1 --cpu_bind=cores ~/exawind/source/tioga_utils/build_gcc_opt/exatioga -i ctv_amr_constant.yaml
+
+echo ""
+echo "RUNNING CTV test with 1 CELL VAR AND 2 NODE VAR ON 1 CORE."
+echo ""
+srun -n 1 -c 1 --cpu_bind=cores ~/exawind/source/tioga_utils/build_gcc_opt/exatioga -i ctv_stk_constant.yaml
+
+echo ""
+echo "RUNNING SINGLE SPHERE TEST ON 1 CORE."
+echo ""
+cd /scratch/asharma/amr_reg_test/cpu_gpu_pathway/sphere_w_amr
+rm -rf core* plt*
+srun -n 1 -c 1 --cpu_bind=cores ~/exawind/source/tioga_utils/build_gcc_opt/exatioga -i sphere.yaml
+
+echo ""
+echo "RUNNING SINGLE SPHERE TEST ON 3 CORES."
+echo ""
+srun -n 3 -c 1 --cpu_bind=cores ~/exawind/source/tioga_utils/build_gcc_opt/exatioga -i sphere.yaml
+
+echo ""
+echo "RUNNING FOUR SPHERE TEST ON 1 CORE."
+echo ""
+srun -n 1 -c 1 --cpu_bind=cores ~/exawind/source/tioga_utils/build_gcc_opt/exatioga -i four_sphere.yaml
+
+echo ""
+echo "RUNNING FOUR SPHERE TEST ON 11 CORES."
+echo ""
+srun -n 11 -c 1 --cpu_bind=cores ~/exawind/source/tioga_utils/build_gcc_opt/exatioga -i four_sphere.yaml
+
+echo ""
+echo "RUNNING NREL 5MW TEST WITH SMALL ABL MESH ON 9 CORES."
+echo ""
+cd /scratch/asharma/amr_reg_test/cpu_gpu_pathway/nrel_5mw
+rm -rf core* plt*
+srun -n 9 -c 1 --cpu_bind=cores ~/exawind/source/tioga_utils/build_gcc_opt/exatioga -i nrel5mw_small.yaml
+
+echo ""
+echo "RUNNING NREL 5MW TEST ON 103 CORES."
+echo ""
+rm -rf core*
+srun -n 103 -c 1 --cpu_bind=cores ~/exawind/source/tioga_utils/build_gcc_opt/exatioga -i nrel5mw.yaml
---
+++ gpu/run_tests_gpu.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+echo ""
+echo "RUNNING CTV test with 2 CELL VAR AND 1 NODE VAR ON 1 CORE."
+echo ""
+cd /scratch/asharma/amr_reg_test/gpu/ctv
+rm -rf core* plt*
+srun -n 1 -c 1 --cpu_bind=cores --gres=gpu:1 ~/exawind/source/tioga_utils/build_cuda_opt/exatioga --kokkos-num-devices=1 -i ctv_amr_constant.yaml
+
+echo ""
+echo "RUNNING CTV test with 1 CELL VAR AND 2 NODE VAR ON 1 CORE."
+echo ""
+srun -n 1 -c 1 --cpu_bind=cores --gres=gpu:1 ~/exawind/source/tioga_utils/build_cuda_opt/exatioga --kokkos-num-devices=1 -i ctv_stk_constant.yaml
+
+echo ""
+echo "RUNNING SINGLE SPHERE TEST ON 1 CORE."
+echo ""
+cd /scratch/asharma/amr_reg_test/gpu/sphere_w_amr
+rm -rf core* plt*
+srun -n 1 -c 1 --cpu_bind=cores --gres=gpu:1 ~/exawind/source/tioga_utils/build_cuda_opt/exatioga --kokkos-num-devices=1 -i sphere.yaml
+
+echo ""
+echo "RUNNING SINGLE SPHERE TEST ON 3 CORES."
+echo ""
+srun -n 3 -c 1 --cpu_bind=cores --gres=gpu:2 ~/exawind/source/tioga_utils/build_cuda_opt/exatioga --kokkos-num-devices=2 -i sphere.yaml
+
+echo ""
+echo "RUNNING FOUR SPHERE TEST ON 1 CORE."
+echo ""
+srun -n 1 -c 1 --cpu_bind=cores --gres=gpu:1 ~/exawind/source/tioga_utils/build_cuda_opt/exatioga --kokkos-num-devices=1 -i four_sphere.yaml
+
+echo ""
+echo "RUNNING FOUR SPHERE TEST ON 11 CORES."
+echo ""
+srun -n 11 -c 1 --cpu_bind=cores --gres=gpu:2 ~/exawind/source/tioga_utils/build_cuda_opt/exatioga --kokkos-num-devices=2 -i four_sphere.yaml
+
+echo ""
+echo "RUNNING NREL 5MW TEST WITH SMALL ABL MESH ON 9 CORES."
+echo ""
+cd /scratch/asharma/amr_reg_test/gpu/nrel_5mw
+rm -rf core* plt*
+srun -n 9 -c 1 --cpu_bind=cores --gres=gpu:2 ~/exawind/source/tioga_utils/build_cuda_opt/exatioga --kokkos-num-devices=2 -i nrel5mw_small.yaml
